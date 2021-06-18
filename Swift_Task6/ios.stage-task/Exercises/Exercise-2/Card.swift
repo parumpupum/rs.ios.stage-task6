b2e948a76cca0d4e57848c82e3ff13b539b557b4
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-06-18 17:56:13 +0300

    Update Card.swift

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
@@ -15,7 +15,7 @@ enum Suit: Int, CaseIterable, Codable {
     case diamonds
 }
 
-enum Value: Int, Codable {
+enum Value: Int, CaseIterable, Codable {
     case six
     case seven
     case eight
@@ -37,17 +37,33 @@ struct Card: CardBaseCompatible {
     }
 
     static func == (lhs: Self, rhs: Self) -> Bool {
-        return false
+        if lhs.suit == rhs.suit && lhs.value == rhs.value { return true }
+        else { return false }
     }
 }
 
 extension Card {
 
     func checkIfCanBeat(card: Card) -> Bool {
-        return false
+        let firstCard = self, secondCard = card
+        var answer = false
+        
+        if ((firstCard.isTrump && secondCard.isTrump) || (!firstCard.isTrump && !secondCard.isTrump)){
+            let firstPower = Value.allCases.firstIndex(where: {$0 == firstCard.value})!
+            let secondPower = Value.allCases.firstIndex(where: {$0 == secondCard.value})!
+            
+            if (firstPower > secondPower) { answer = true }
+            else { answer = false }
+        }
+        if (firstCard.isTrump && !secondCard.isTrump){
+            answer = true
+        }
+        if (!firstCard.isTrump && secondCard.isTrump){
+            answer = false
+        }
+        
+        return answer
+        
     }
-
-    func checkValue(card: Card) -> Bool {
-        return false
-    }
+    
 }
