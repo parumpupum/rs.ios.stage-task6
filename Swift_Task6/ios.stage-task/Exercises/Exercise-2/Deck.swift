5a4200988b0c3a8a3451c9bd417188ba17db72c1
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-06-18 17:57:11 +0300

    Update Deck.swift

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Deck.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Deck.swift
@@ -28,26 +28,58 @@ extension Deck {
 
     init(with type: DeckType) {
         self.type = type
+        let suits = Suit.allCases
+        let values = Value.allCases
+        cards = createDeck(suits: suits, values: values)
     }
 
     public func createDeck(suits:[Suit], values:[Value]) -> [Card] {
-        []
+        
+        var cards = [Card]()
+        for suit in suits{
+            for value in values{
+                cards.append(Card(suit: suit, value: value))
+            }
+        }
+        
+        return cards
+        
     }
 
-    public func shuffle() {
-
+    public mutating func shuffle() {
+        var newDeck = cards
+        cards.removeAll()
+        
+        while let newCard = newDeck.randomElement(){
+            
+            cards.append(newCard)
+            let i = newDeck.firstIndex(of: newCard)!
+            newDeck.remove(at: i)
+            
+        }
+        
     }
 
-    public func defineTrump() {
-
+    public mutating func defineTrump() {
+        trump = cards.last?.suit
+        
+        for (index, card) in cards.enumerated(){
+            if card.suit == trump{
+                cards[index].isTrump = true
+            }
+        }
     }
 
-    public func initialCardsDealForPlayers(players: [Player]) {
-
+    public mutating func initialCardsDealForPlayers(players: [Player]) {
+        for i in 0..<players.count{
+            var newHand = [Card]()
+            for q in 1...6{
+                newHand.append(cards.first!)
+                cards.removeFirst()
+            }
+            players[i].hand = newHand
+        }
     }
-
-    public func setTrumpCards(for suit:Suit) {
-
-    }
+    
 }
 
