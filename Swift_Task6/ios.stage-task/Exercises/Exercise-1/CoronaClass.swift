6bed8fc3d9b62e6004b0bffd91102a2b03f94d27
Kirill Hobyan <82779687+parumpupum@users.noreply.github.com>
2021-06-18 15:07:35 +0300

    Update CoronaClass.swift

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -3,16 +3,65 @@ import Foundation
 class CoronaClass {
  
      var seats = [Int]()
-
+     var seatsCount : Int
+    
      init(n: Int) {
- 
+        seatsCount = n
      }
      
      func seat() -> Int {
-        -1
+        if seats.count == 0 {
+            seats.append(0)
+            return 0
+        }
+        
+        var maxDistance = -1, seat = -1, position = -1
+        for i in 0..<seats.count - 1{
+            
+            let possibleSeat = (seats[i + 1] + seats[i]) / 2
+            if min(possibleSeat - seats[i], seats[i + 1] - possibleSeat) > maxDistance{
+                
+                maxDistance = min(possibleSeat - seats[i], seats[i + 1] - possibleSeat)
+                seat = possibleSeat
+                position = i + 1
+                
+            }
+            
+        }
+        
+        if seats[0] - 1 > maxDistance{
+            
+            maxDistance = seats[0] - 1
+            seat = 0
+            position = 0
+            
+        }
+        
+        if seatsCount - 2 - seats[seats.count - 1] > maxDistance{
+            
+            maxDistance = seatsCount - 2 - seats[seats.count - 1]
+            seat = seatsCount - 1
+            position = seats.count
+            
+        }
+        
+        seats.insert(seat, at: position)
+        return seat
+        
      }
      
      func leave(_ p: Int) {
-         
+        
+        for i in 0..<seats.count{
+            
+            if seats[i] == p {
+                
+                seats.remove(at: i)
+                return
+                
+            }
+            
+        }
+        
      }
 }
